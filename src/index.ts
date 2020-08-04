6f8717ce78c8c5f2561ae33384c698abc63dc19d
Carter Himmel <carter@elhnet.net>
2020-08-03 20:55:36 -0600

    docs: comments

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,51 @@
 import { Amqp } from '@spectacles/brokers';
-import { User, Message, Ready } from '@spectacles/types';
+import { Message, Ready } from '@spectacles/types';
 import Proxy, { StringRecord, Request } from './proxy';
 
+// importing all our env vars for later use
 const { REST_GROUP, GATEWAY_GROUP, REST_EVENT, AMQP_URL, DISCORD_TOKEN, PREFIX } = process.env as StringRecord;
 
+// initialize our proxy class
 const proxy = new Proxy({ group: REST_GROUP, event: REST_EVENT, url: AMQP_URL });
+// initialize a broker to the gateway
 const gateway = new Amqp(GATEWAY_GROUP);
 
+// the default headers for our requests
 const DEFAULT_HEADERS = {
 	Authorization: `Bot ${DISCORD_TOKEN}`,
 	'Content-Type': 'application/json',
 }
 
+// data for creating a new message
 interface MessageData {
 	content: string;
 }
 
+// creating a new message
 async function sendMessage(channelId: string, data: MessageData | string): Promise<Message> {
+	// the options for the request
 	const request: Request = {
 		method: 'POST',
 		path: `/channels/${channelId}/messages`,
 		headers: DEFAULT_HEADERS,
 	};
 
+	// so the second param can be a string identified as
+	// the message content
 	if (typeof data === 'string') request.body = { content: data };
 	// @ts-ignore
 	else request.body = data;
 
-	const res = await proxy.request<Message>(request);
+	// finally perform our request
+	const { body } = await proxy.request<Message>(request);
 
-	if (typeof res.body === 'string') throw Error(res.body);
-	return res.body.body as Message;
+	// if the body here is a string, the request failed
+	if (typeof body === 'string') throw Error(body);
+	return body.body;
 }
 
+// edit a message, basically everything from above
+// is same semantically
 async function editMessage(channelId: string, messageId: string, data: MessageData | string): Promise<Message> {
 	const request: Request = {
 		method: 'PATCH',
@@ -50,8 +63,9 @@ async function editMessage(channelId: string, messageId: string, data: MessageDa
 	return res.body.body as Message;
 }
 
+// handle a message from the gateway
 async function handleMessage(msg: Message): Promise<Message | void> {
-	if (msg.author.bot)
+	if (msg.author.bot) return; // if the message is from a bot, ignore!
 	if (!msg.content.startsWith(PREFIX)) return; // return if the message doesn't start with the command prefix
 
 	const args = msg.content.slice(PREFIX.length).split(/ +/);
@@ -64,27 +78,32 @@ async function handleMessage(msg: Message): Promise<Message | void> {
 	}
 }
 
-
+// bootstrap our bot
 async function bootstrap() {
+	// creating the proxies connection to rabbit
 	await proxy.connect();
 	console.debug(`[PROXY] Connected to ${proxy._connection?.connection.serverProperties.cluster_name}.`);
 
+	// listen on the ready event
 	gateway.on('READY', ({ user }: Ready, { ack }) => {
 		ack();
 
 		console.log(`${user.username}${user.discriminator} (${user.id}) has emitted READY.`);
 	});
 
+	// listen on the message event and handle new messages
 	gateway.on('MESSAGE_CREATE', (data: Message, { ack }) => {
 		ack();
 
 		return handleMessage(data);
 	});
 
+	// connect to rabbit
 	const { connection } = await gateway.connect(process.env.AMQP_URL);
 	console.debug(`[GATEWAY] Connected to ${connection.serverProperties.cluster_name}.`);
 
 	const events = ['READY', 'MESSAGE_CREATE'];
+	// subscribe to only the events we need
 	await gateway.subscribe(events);
 	console.log(`Subscribed to events ${events.join(', ')}.`);
 }
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,11 @@
 import { Amqp } from '@spectacles/brokers';
 import { once } from 'events';
 
+// shorthand! 
 export type StringRecord = Record<string, string>;
 
+// the options for our request
+// https://github.com/spec-tacles/proxy#request-format
 export interface Request {
 	method: string;
 	path: string;
@@ -11,6 +14,8 @@ export interface Request {
 	headers: StringRecord;
 }
 
+// the response statuses per
+// https://github.com/spec-tacles/proxy#response-status
 export enum ResponseStatus {
 	SUCCESS,
 	UNKNOWN_ERROR,
@@ -22,12 +27,16 @@ export enum ResponseStatus {
 	REQUEST_FAILURE,
 }
 
+// the response object
+// https://github.com/spec-tacles/proxy#response-format
 export interface Response<T> {
 	status: ResponseStatus;
 	// will only be a string if the Response#status is non-zero
 	body: ResponseBody<T>  | string;
 }
 
+// the actual response body 
+// https://github.com/spec-tacles/proxy#response-body
 export interface ResponseBody<T> {
 	status: number;
 	headers: StringRecord;
@@ -35,6 +44,7 @@ export interface ResponseBody<T> {
 	body: T;
 }
 
+// the options for initializing our proxy class
 export interface ProxyOptions {
 	event: string;
 	group: string;
@@ -50,11 +60,18 @@ export default class Proxy {
 		this.broker.on('error', console.error);
 	}
 
+	/**
+	 * Performs an RPC call to the proxy
+	 * @param request The request data
+	 */
 	public async request<T = any>(request: Request): Promise<Response<T>> {
 		const response = await this.broker.call(this.options.event, request);
 		return response;
 	}
 
+	/**
+	 * Connects to the RPC 
+	 */
 	public async connect(): Promise<this> {
 		const connection = await this.broker.connect(this.options.url);
 		this._connection = connection;
